00672fb49762540cd6314908ae4c7145019c6893
AFuriouscat <65242633+AFuriouscat@users.noreply.github.com>
2024-11-17 06:34:00 +0800

    Add files via upload

--- startGame.js
+++ startGame.js
@@ -1,11 +1,11 @@
 const request = require('request');
 
 const botToken = '8156250890:AAElfC9mxLv3FYLnqzhDEM5U7Q0F6a13IMg';
-//const chatId = '6251179385';
-//const gameUrl = 'https://t.me/virtualRobotPetsBot?game=VirtualPetRobot';
+const chatId = '6251179385';
+const gameUrl = 't.me/TONDroid_Bot/TONDroid';
 
-const chatId = 'USER_CHAT_ID';
-const gameUrl = 'https://your-game-url.com';
+//const chatId = 'USER_CHAT_ID';
+//const gameUrl = 'https://your-game-url.com';
 
 const url = `https://api.telegram.org/bot${botToken}/sendGame?chat_id=${chatId}&game_short_name=your_game_short_name&game_url=${gameUrl}`;
 
